63db2406fcb30a5d6b18d2ed6d026ea2d366ed70
Mateus Castro <mcastro.mateus@gmail.com>
2020-08-25 22:29:16 -0300

    Projeto Iniciado.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Conversor de Moedas
+
+Projeto de aplicação para conversão de moedas, utilizando Express, EJS e Bootstrap.
+
+É possível informar os valores da cotação do Dólar, cotação do Euro e o montante em Reais que se deseja converter, após o cálculo de conversão são exibidos os valores do montante informado convertido em Dólar e Euro.
+
+# ScreenShots
+## Intro
+![Alt text](/screenshots/001.jpg?raw=true "Introdução.")
+## Valores Convertidos
+![Alt text](/screenshots/002.jpg?raw=true "Valores Convertidos.")
+## Apresentação de Erros
+![Alt text](/screenshots/003.jpg?raw=true "Apresentação de Erros.")
+
+# Instrucões
+### Rodar Aplicação.
+```
+npm run serve
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index", {cotacao_dolar: "", cotacao_euro: "", valor: "", valor_dolar: "", valor_euro: "", mensagens: []});
+} );
+
+app.post("/converter", function(req, res){
+    let mensagens = [];
+
+    if(!req.body.valor)
+      mensagens.push("Montante em Reais não informado.");
+
+    if(!req.body.cotacao_dolar)
+      mensagens.push("Cotação do Dólar não informada.");
+
+    if(!req.body.cotacao_euro)
+      mensagens.push("Cotação do Euro não informada.");
+
+    let valor = parseFloat(req.body.valor);
+    let cotacao_dolar = parseFloat(req.body.cotacao_dolar);
+    let cotacao_euro = parseFloat(req.body.cotacao_euro);
+    let valor_dolar = valor * (1 / cotacao_dolar);
+    let valor_euro = valor * (1 / cotacao_euro);
+    
+    res.render("index", {valor: valor.toFixed(2), cotacao_dolar: cotacao_dolar.toFixed(2), cotacao_euro: cotacao_euro.toFixed(2), valor_dolar: valor_dolar.toFixed(2), valor_euro: valor_euro.toFixed(2), mensagens: mensagens});
+});
+
+app.listen(3000);
